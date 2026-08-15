2a1ff46c8a70d613e3dad66ce5312dc4c88fdba0
Stevano016 <stevanowahyu016@gmail.com>
2026-08-15 19:21:55 +0700

    Fix syntax parse error: add missing closing div in DPT pagination block

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1204,6 +1204,7 @@ export default function App() {
                     >
                       Selanjutnya
                     </button>
+                  </div>
                 </div>
               </>
             )}
