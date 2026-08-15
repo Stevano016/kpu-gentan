7e40cd202b3af853919b60e5bd23ad9aab9b4e57
Stevano016 <stevanowahyu016@gmail.com>
2026-08-15 19:34:06 +0700

    Update DatabaseSeeder: seed both DPT and DPK voters, and assign sequential unique voter IDs starting with USH-GTN-026

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -82,40 +82,50 @@ class DatabaseSeeder extends Seeder
             'tps_id' => 3,
         ]);
 
-        // 4. Create DPT voters
-        $dpts = [
-            // TPS 01
-            ['nik' => '3311010101010001', 'nama' => 'Budi Santoso', 'tps_id' => 1],
-            ['nik' => '3311010101010002', 'nama' => 'Siti Aminah', 'tps_id' => 1],
-            ['nik' => '3311010101010003', 'nama' => 'Eko Prasetyo', 'tps_id' => 1],
-            ['nik' => '3311010101010004', 'nama' => 'Dewi Lestari', 'tps_id' => 1],
-            ['nik' => '3311010101010005', 'nama' => 'Ahmad Fauzi', 'tps_id' => 1],
-            
-            // TPS 02
-            ['nik' => '3311020202020001', 'nama' => 'Rian Hidayat', 'tps_id' => 2],
-            ['nik' => '3311020202020002', 'nama' => 'Santi Wijaya', 'tps_id' => 2],
-            ['nik' => '3311020202020003', 'nama' => 'Gita Gutawa', 'tps_id' => 2],
-            ['nik' => '3311020202020004', 'nama' => 'Andi Hermawan', 'tps_id' => 2],
-            ['nik' => '3311020202020005', 'nama' => 'Sri Wahyuni', 'tps_id' => 2],
-            
-            // TPS 03
-            ['nik' => '3311030303030001', 'nama' => 'Joko Widodo', 'tps_id' => 3],
-            ['nik' => '3311030303030002', 'nama' => 'Megawati Sukarno', 'tps_id' => 3],
-            ['nik' => '3311030303030003', 'nama' => 'Prabowo Subianto', 'tps_id' => 3],
-            ['nik' => '3311030303030004', 'nama' => 'Anies Baswedan', 'tps_id' => 3],
-            ['nik' => '3311030303030005', 'nama' => 'Ganjar Pranowo', 'tps_id' => 3],
+        // 4. Create voters (DPT & DPK)
+        $voters = [
+            // TPS 01 (DPT)
+            ['nik' => '3311010101010001', 'nama' => 'Budi Santoso', 'tps_id' => 1, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311010101010002', 'nama' => 'Siti Aminah', 'tps_id' => 1, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311010101010003', 'nama' => 'Eko Prasetyo', 'tps_id' => 1, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311010101010004', 'nama' => 'Dewi Lestari', 'tps_id' => 1, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311010101010005', 'nama' => 'Ahmad Fauzi', 'tps_id' => 1, 'jenis_pemilih' => 'dpt'],
+            // TPS 01 (DPK)
+            ['nik' => '3311010101019001', 'nama' => 'Rudi Salim (DPK)', 'tps_id' => 1, 'jenis_pemilih' => 'dpk'],
+            ['nik' => '3311010101019002', 'nama' => 'Lani Marlina (DPK)', 'tps_id' => 1, 'jenis_pemilih' => 'dpk'],
+
+            // TPS 02 (DPT)
+            ['nik' => '3311020202020001', 'nama' => 'Rian Hidayat', 'tps_id' => 2, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311020202020002', 'nama' => 'Santi Wijaya', 'tps_id' => 2, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311020202020003', 'nama' => 'Gita Gutawa', 'tps_id' => 2, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311020202020004', 'nama' => 'Andi Hermawan', 'tps_id' => 2, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311020202020005', 'nama' => 'Sri Wahyuni', 'tps_id' => 2, 'jenis_pemilih' => 'dpt'],
+            // TPS 02 (DPK)
+            ['nik' => '3311020202029001', 'nama' => 'Ferry Sunarto (DPK)', 'tps_id' => 2, 'jenis_pemilih' => 'dpk'],
+            ['nik' => '3311020202029002', 'nama' => 'Yuni Shara (DPK)', 'tps_id' => 2, 'jenis_pemilih' => 'dpk'],
+
+            // TPS 03 (DPT)
+            ['nik' => '3311030303030001', 'nama' => 'Joko Widodo', 'tps_id' => 3, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311030303030002', 'nama' => 'Megawati Sukarno', 'tps_id' => 3, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311030303030003', 'nama' => 'Prabowo Subianto', 'tps_id' => 3, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311030303030004', 'nama' => 'Anies Baswedan', 'tps_id' => 3, 'jenis_pemilih' => 'dpt'],
+            ['nik' => '3311030303030005', 'nama' => 'Ganjar Pranowo', 'tps_id' => 3, 'jenis_pemilih' => 'dpt'],
+            // TPS 03 (DPK)
+            ['nik' => '3311030303039001', 'nama' => 'Muhaimin Iskandar (DPK)', 'tps_id' => 3, 'jenis_pemilih' => 'dpk'],
+            ['nik' => '3311030303039002', 'nama' => 'Mahfud MD (DPK)', 'tps_id' => 3, 'jenis_pemilih' => 'dpk'],
         ];
 
         $index = 1;
-        foreach ($dpts as $d) {
+        foreach ($voters as $v) {
             $suffix = str_pad($index, 4, '0', STR_PAD_LEFT);
             $idPemilih = 'USH-GTN-026' . $suffix;
             Dpt::create([
-                'nik' => $d['nik'],
-                'nama' => $d['nama'],
-                'tps_id' => $d['tps_id'],
+                'nik' => $v['nik'],
+                'nama' => $v['nama'],
+                'tps_id' => $v['tps_id'],
                 'status_hadir' => false,
                 'waktu_checkin' => null,
+                'jenis_pemilih' => $v['jenis_pemilih'],
                 'id_pemilih' => $idPemilih,
                 'qr_payload' => $idPemilih,
             ]);
